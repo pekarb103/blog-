cbfa6ddc217b3563b3b14c0d5b3e0d291248b726
pekarb103 <pekarb103@tut.by>
2015-01-07 21:31:57 +0300

    вроде как все с валидацией)

--- MvcApplication1/MvcApplication1/Models/UserModel.cs
+++ MvcApplication1/MvcApplication1/Models/UserModel.cs
@@ -3,8 +3,6 @@ namespace MvcApplication1.Models
 {
     public class UserModel
     {
-        [Required(ErrorMessage = "Пожалуйста,введите {0}!")]
-        [Display(Name = "Возраст")]
         [Range(4, 100)]
         public int? Age { get; set; }
         public string Username { get; set; }
